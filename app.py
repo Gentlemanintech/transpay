a0e3d939da6d943dbb00ca7e2144e95943a57317
Gentlemanintech <umaidoki42@gmail.com>
2024-07-07 22:00:50 +0100

    made some changeson the layout.html file

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
     heading = "Did he get the"
     para = "This is my Webpage"
 
-    return render_template("index.html", heading=heading, para=para)
+    return render_template("layout.html", heading=heading, para=para)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
