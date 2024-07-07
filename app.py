656755d58abf58f93cb67ccefe7445deae3d8bef
Gentlemanintech <umaidoki42@gmail.com>
2024-07-07 19:58:54 +0100

    Added files to the folder

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    heading = "Did he get the"
+    para = "This is my Webpage"
+
+    return render_template("index.html", heading=heading, para=para)
